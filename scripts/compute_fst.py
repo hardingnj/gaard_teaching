2c4c0595259c724ee7d702e395eb09f048462645
Nicholas Harding <njh@well.ox.ac.uk>
2018-01-09 05:50:29 +0000

    new scripts

---
+++ scripts/compute_fst.py
@@ -0,0 +1,72 @@
+__author__ = 'Nicholas Harding'
+
+import pandas as pd
+import numpy as np
+import h5py
+import allel
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("vcf", default=None)
+parser.add_argument("out", default=None)
+
+parser.add_argument("--windowsize", "-w", default=20000, type=int)
+parser.add_argument("--chrom", "-c", default=None, required=True)
+
+parser.add_argument("--samplesA", required=True)
+parser.add_argument("--samplesB", required=True)
+args = parser.parse_args()
+
+with open(args.samplesA, "r") as reader:
+    samplesA = [x.rstrip() for x in reader.readlines()]
+with open(args.samplesB, "r") as reader:
+    samplesB = [x.rstrip() for x in reader.readlines()]
+
+fh = allel.read_vcf(args.vcf)
+
+def main(callset, samplesA, samplesB, window_size):
+
+    callset_samples = callset["samples"][:].astype("U8").tolist()
+
+    sa = [callset_samples.index(x) for x in samplesA if x in callset_samples]
+    sb = [callset_samples.index(x) for x in samplesB if x in callset_samples]
+
+
+    assert len(sa) > 0, "No samples in A"
+    if len(sa) < len(samplesA):
+        print("Not all samples provided in A were found. Found {0}/{1}".format(len(sa), len(samplesA)))
+    assert len(sb) > 0, "No samples in B"
+    if len(sb) < len(samplesB):
+        print("Not all samples provided in B were found. Found {0}/{1}".format(len(sb), len(samplesB)))
+
+    positions = allel.SortedIndex(callset["variants/POS"])
+    
+    last_pos = positions[-1]
+    window_starts = np.arange(1, last_pos, window_size, dtype=int)
+    df = pd.DataFrame(columns=["start", "stop", "nvar"], index=window_starts)
+    df["fst"] = pd.Series(index=window_starts, dtype=float)
+
+    df["start"] = window_starts
+    df["stop"] = window_starts + window_size
+
+    for start in window_starts:
+        try:
+            loc = positions.locate_range(start, start + window_size - 1) 
+        except KeyError:
+            df.at[start, "nvar"] = 0
+            continue
+
+        g = allel.GenotypeChunkedArray(callset["calldata/GT"][loc])
+
+        ac1 = g.count_alleles(subpop=sa)
+        ac2 = g.count_alleles(subpop=sb)
+        
+        num, den = allel.stats.hudson_fst(ac1, ac2)
+        df.at[start, "fst"] = (np.sum(num) / np.sum(den))
+        df.at[start, "nvar"] = num.size
+
+    return df
+
+result = main(callset=fh, samplesA=samplesA, samplesB=samplesB, window_size=args.windowsize)
+result.to_csv(args.out, sep="\t", index=False)
---
+++ scripts/run_fst.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+python scripts/compute_fst.py \
+  /kwiat/vector/ag1000g/release/phase1.AR3.1/haplotypes/specific_regions/PARA/2L_2358158_2431617.vcf.gz \
+  output/fst.txt \
+  --samplesA samples/phase2.ar1.GW.txt \
+  --samplesB samples/phase2.ar1.KE.txt \
+  --chrom 2L
